395ea02ce59b29c76d413ad4413b8db5301c79a7
NinaKRaju <ninakraju@gmail.com>
2026-02-11 22:49:00 -0500

    Update TreeComparator.java

--- TreeComparator.java
+++ TreeComparator.java
@@ -1,9 +1,17 @@
 import java.util.Comparator;
 
-public class TreeComparator implements Comparator<BinaryTree<Character>>{
+public class TreeComparator implements Comparator<BinaryTree<CodeTreeElement>>{
 
     @Override
-    public int compare(BinaryTree<Character> o1, BinaryTree<Character> o2) {
-        return 0;
+    public int compare(BinaryTree<CodeTreeElement> o1, BinaryTree<CodeTreeElement> o2) {
+        if(o1.getData().getFrequency().equals(o2.getData().getFrequency())){
+            return 0;
+        }
+        else if(o1.getData().getFrequency() > o2.getData().getFrequency()){
+            return 1;
+        }
+        else{
+            return -1;
+        }
     }
 }
