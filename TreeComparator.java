f5074543838b69a837a73dbc6d55b0d54b4f19c8
NinaKRaju <ninakraju@gmail.com>
2026-02-11 22:42:10 -0500

    Update TreeComparator.java

--- TreeComparator.java
+++ TreeComparator.java
@@ -1,38 +1,9 @@
-import net.datastructures.EmptyPriorityQueueException;
-import net.datastructures.Entry;
-import net.datastructures.InvalidKeyException;
-import net.datastructures.PriorityQueue;
-
 import java.util.Comparator;
 
-public class TreeComparator implements PriorityQueue, Comparator {
-    @Override
-    public int size() {
-        return 0;
-    }
+public class TreeComparator implements Comparator<BinaryTree<Character>>{
 
     @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public Entry min() throws EmptyPriorityQueueException {
-        return null;
-    }
-
-    @Override
-    public Entry insert(Object o, Object o2) throws InvalidKeyException {
-        return null;
-    }
-
-    @Override
-    public Entry removeMin() throws EmptyPriorityQueueException {
-        return null;
-    }
-
-    @Override
-    public int compare(Object o1, Object o2) {
+    public int compare(BinaryTree<Character> o1, BinaryTree<Character> o2) {
         return 0;
     }
 }
